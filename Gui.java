9149399526db8a8cd26494288a93617714e3cfc7
Chad Smith <chad.j.smith@gmail.com>
2013-05-30 00:21:46 -0400

    Updates & Screenshots
    Changed the output into two JLists, putting the found subdomains in the
    top box, and the corresponding IP addresses in the box below...the IP
    list changes depending which subdomain is selected.

--- Gui.java
+++ Gui.java
@@ -1,6 +1,8 @@
 package com.viralrain.dns;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.*;
 import java.awt.*;
 import java.beans.PropertyChangeEvent;
@@ -12,12 +14,16 @@ public class Gui extends JFrame{
 
     public static JTextField hostname;      // input text field
     public static JTextArea output;         // output text area
+    public static DefaultListModel<SubDomain> outputSubsList = new DefaultListModel<SubDomain>();
+    public static JList<SubDomain> outputSubs = new JList<SubDomain>(outputSubsList);
+    public static DefaultListModel<IPs> outputAddrssList = new DefaultListModel<IPs>();
+    public static JList<IPs> outputAddrss = new JList<IPs>(outputAddrssList);
     public static JLabel status;            // status label
     public static JProgressBar progress;    // progress bar
     public static JButton lookup;           // lookup button
 
     // get the list of sub-domains
-    public static String[] subs = FileIO.getList( "subs.txt" );
+    public static String[] subs = FileIO.getList( "subs-test.txt" );
 
     // create the window
     public Gui(){
@@ -30,13 +36,33 @@ public class Gui extends JFrame{
             JLabel inLbl = new JLabel( "Host Name" );
             hostname = new JTextField();                // input text field
             hostname.setToolTipText("Test");
+            hostname.setBorder( BorderFactory.createLineBorder( Color.lightGray ));
         input.add(inLbl);
         input.add( hostname );
 
-        // output text area
-        output = new JTextArea();
-        JScrollPane jsp = new JScrollPane(output);
-        jsp.setBorder( BorderFactory.createLineBorder( Color.lightGray ) );
+        // output subdomain list
+
+        outputSubs.addListSelectionListener( new ListListener() );
+        outputSubs.setCellRenderer( new SubListRenderer() );
+        JScrollPane jsp1 = new JScrollPane( outputSubs );
+        jsp1.setBorder( BorderFactory.createCompoundBorder(
+                BorderFactory.createMatteBorder(0, 0, 5, 0, this.getBackground()),
+                BorderFactory.createLineBorder(Color.lightGray)
+        ));
+
+        // output ip address list
+        outputAddrss.setCellRenderer( new IPListRenderer() );
+        JScrollPane jsp2 = new JScrollPane( outputAddrss );
+        jsp2.setBorder( BorderFactory.createLineBorder( Color.lightGray ) );
+        jsp2.setBorder( BorderFactory.createCompoundBorder(
+                BorderFactory.createMatteBorder(0, 0, 0, 0, this.getBackground()),
+                BorderFactory.createLineBorder(Color.lightGray)
+        ));
+
+        JPanel outputs = new JPanel();
+        outputs.setLayout(new GridLayout(2, 1));
+        outputs.add( jsp1 );
+        outputs.add( jsp2 );
 
         // setup buttons panel, with padding, grid layout
         JPanel buttons = new JPanel();
@@ -54,7 +80,7 @@ public class Gui extends JFrame{
         statusBar.setLayout(new GridLayout(1, 2, 5, 5));
         statusBar.setPreferredSize(new Dimension(100, 23));
         statusBar.setBorder( BorderFactory.createCompoundBorder(
-                    BorderFactory.createLoweredSoftBevelBorder(),
+                    BorderFactory.createLineBorder(Color.lightGray),
                     BorderFactory.createMatteBorder( 2, 2, 2, 2, this.getBackground() )
                 ));
             status = new JLabel( "Idle" );
@@ -68,15 +94,46 @@ public class Gui extends JFrame{
         main.setLayout(new BorderLayout());
         main.setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, this.getBackground()));
         main.add( input, BorderLayout.NORTH );
-        main.add( jsp, BorderLayout.CENTER );
+        main.add( outputs, BorderLayout.CENTER );
         main.add( buttons, BorderLayout.SOUTH );
 
+        JMenuBar topMenu = new JMenuBar();
+
+        JMenu fileMenu = new JMenu( "File" );
+            fileMenu.add( makeMenuItem("Exit") );
+
+        JMenu editMenu = new JMenu( "Edit" );
+            editMenu.add( makeMenuItem( "Clear" ) );
+
+        topMenu.add(fileMenu);
+        topMenu.add(editMenu);
+        this.setJMenuBar( topMenu );
+
+
         // setup window options and add stuff to window
-        this.setLayout( new BorderLayout() );
+        this.setLayout(new BorderLayout());
         this.add( main, BorderLayout.CENTER );
         this.add( statusBar, BorderLayout.SOUTH );
         this.setSize( 350, 500 );
         this.setLocationRelativeTo( null );
+    }
+
+    private JMenuItem makeMenuItem(String name){
+        JMenuItem m = new JMenuItem( name );
+        m.addActionListener( new MenuListener() );
+        return m;
+    }
+
+    private void clearGUI(){
+        // clear the output and input texts
+        hostname.setText( "" );
+        outputAddrssList.removeAllElements();
+        outputAddrss.removeAll();
+        outputSubsList.removeAllElements();
+        outputSubsList.clear();
+        outputSubs.removeAll();
+        progress.setValue(0);
+        status.setText("Idle");
     }
 
     // click listener for the lookup button
@@ -87,8 +144,7 @@ public class Gui extends JFrame{
             Lookup task = new Lookup();
             // reset progress bar
             progress.setValue( 0 );
-            // create header for lookup output
-            output.append( "----- Looking up hosts on: " + hostname.getText() + " -----\n" );
+
             // add listener for thread and execute the task
             task.addPropertyChangeListener(this);
             task.execute();
@@ -105,15 +161,71 @@ public class Gui extends JFrame{
         }
     }
 
+    private class ListListener implements ListSelectionListener {
+        @Override
+        public void valueChanged(ListSelectionEvent lse) {
+            if( !outputSubs.isSelectionEmpty() ){
+                int cur = outputSubs.getSelectedIndex();
+                outputAddrssList.clear();
+                outputAddrss.removeAll();
+                for( int i = 0; i < outputSubsList.get( cur ).ipList.getSize(); i++ ){
+                    outputAddrssList.addElement( outputSubsList.get( cur ).ipList.get( i ) );
+                }
+            }
+        }
+    }
+
     // click listener for the clear button
     private class ClearListener implements ActionListener {
         @Override
         public void actionPerformed( ActionEvent e ) {
-            // clear the output and input texts
-            hostname.setText( "" );
-            output.setText( "" );
-            progress.setValue(0);
-            status.setText("Idle");
+            clearGUI();
+        }
+    }
+
+    private class MenuListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if( e.getActionCommand().equals("Clear")){
+            clearGUI();
+            }
+            if( e.getActionCommand().equals("Exit")){
+                System.exit(0);
+            }
+        }
+    }
+    private class SubListRenderer extends DefaultListCellRenderer{
+        public Component getListCellRendererComponent(
+                JList<?> list,
+                Object value,
+                int index,
+                boolean iss,
+                boolean chf
+        ){
+            super.getListCellRendererComponent(list, value, index, iss, chf);
+
+            SubDomain subd = (SubDomain)value;
+            setText( subd.getSubName() );
+
+            return this;
+        }
+    }
+
+    private class IPListRenderer extends DefaultListCellRenderer{
+        public Component getListCellRendererComponent(
+                JList<?> list,
+                Object value,
+                int index,
+                boolean iss,
+                boolean chf
+        ){
+            super.getListCellRendererComponent(list, value, index, iss, chf);
+
+            IPs ip = (IPs)value;
+            setText( ip.getIP() );
+
+            return this;
         }
     }
 }
--- Lookup.java
+++ Lookup.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Iterator;
 
 // class: Lookup
 // purpose: thread for looking up DNS hosts
@@ -26,19 +27,23 @@ public class Lookup extends SwingWorker<Void, Void> {
         int p = 0;
         double max = Gui.subs.length;
 
+        SubDomain subd = new SubDomain("");
+
         // for each subdomain in the list
         for ( String sub : Gui.subs ) {
             p++;
             // lookup the current sub-domain
             try{
                 curAddresses = DNSLookup( sub + '.' + domain );
+                subd = new SubDomain( sub + '.' + domain );
+                Gui.outputSubsList.addElement( subd );
             }catch( UnknownHostException he ){
                 curAddresses = new String[0];
             }
 
             // output the found sub-domain addresses
-            for( int i = 0; i < curAddresses.length; i++ ){
-                Gui.output.append(curAddresses[i] + "\n");
+            for( String cur : curAddresses ){
+                subd.ipList.addElement( new IPs( cur ) );
             }
             // increment the progress bar
             setProgress( (int)((p / max)*100) );
@@ -52,9 +57,8 @@ public class Lookup extends SwingWorker<Void, Void> {
     @Override
     protected void done(){
         Toolkit.getDefaultToolkit().beep();
-        Gui.lookup.setEnabled( true );
-        Gui.status.setText( "Done" );
-        Gui.output.append( "----- Finished! -----\n\n" );
+        Gui.lookup.setEnabled(true);
+        Gui.status.setText("Done");
     }
 
     // DNSLookup
@@ -70,5 +74,34 @@ public class Lookup extends SwingWorker<Void, Void> {
             output[i] = addresses[i].toString();
         }
         return output;
+    }
+}
+
+class SubDomain{
+    public DefaultListModel<IPs> ipList= new DefaultListModel<IPs>();
+    public String sub;
+
+    public SubDomain( String sub ){
+        this.sub = sub;
+    }
+
+    public void addSub( IPs ip ){
+        ipList.addElement( ip );
+    }
+
+    public String getSubName(){
+        return this.sub;
+    }
+}
+
+class IPs{
+    String ip;
+
+    public IPs( String ip ){
+        this.ip = ip;
+    }
+
+    public String getIP(){
+        return this.ip;
     }
 }
